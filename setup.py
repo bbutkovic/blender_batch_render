e4f20a34047c2731c4fa7d578a65d6ce81ac5331
Borna Butkovic <bornabutkovic@gmail.com>
2019-02-26 10:44:06 +0100

    Fix mistake in setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 setup(name='blender_batch_render',
       version='0.1',
       description='Tool for rendering Blender models in batch with every supplied texture.',
-      url='http://github.com/bbutkovic/funniest',
+      url='http://github.com/bbutkovic/blender_batch_render',
       author='Borna Butkovic',
       author_email='bornabutkovic@gmail.com',
       license='MIT',
